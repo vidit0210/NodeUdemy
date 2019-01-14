102cfac44403a9bb0789a48058f706cacaea1f0a
Vidit Shah <vidit0210@gmail.com>
2019-01-15 01:45:51 +0530

    stats

--- Express/app.js
+++ Express/app.js
@@ -1,16 +1,18 @@
 const express = require("express"); //exports a function
 const app = express();
 const http = require("http");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const ShopRoutes = require("./routes/shop");
 // app.use((req, res, next) => {
 //   console.log("In the MiddleWare");
 //   next(); // Allows the request to the next middleware
 // });
-
-app.use("/add-product", (req, res) => {
-  res.send("<p>Add Product Page</p>");
-});
-app.use("/", (req, res) => {
-  res.send("Default Page");
+app.use(bodyParser.urlencoded({ extended: false })); // Handles request body Parsing
+app.use(adminRoutes);
+app.use(ShopRoutes);
+app.use("/", (req, res, nex) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
 });
 
 // app.use((req, res, next) => {
