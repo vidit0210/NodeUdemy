66573ed002a7782efe25a1a8d61bfda88e7dc110
Vidit Shah <vidit0210@gmail.com>
2019-01-15 12:51:54 +0530

    port

--- Express/app.js
+++ Express/app.js
@@ -15,11 +15,12 @@ app.use("/", (req, res, nex) => {
   res.status(404).send("<h1>Page Not Found</h1>");
 });
 
+
 // app.use((req, res, next) => {
 //   console.log("Came from Initial Middleware");
 //   res.send("<h2>Hello from Express</h2>");
 // });
-
-app.listen(3003, () => {
+const PORT = 3003;
+app.listen(PORT, () => {
   console.log("Listening to The 3003");
 }); //Creates Port and Listen to the Server
