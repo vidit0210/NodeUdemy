48836f81d35e032c146407e72702f8914d2ed964
Vidit Shah <vidit0210@gmail.com>
2019-04-05 16:32:51 +0530

    Refactor

--- Basics/sha.js
+++
@@ -1,7 +0,0 @@
-const SHA256 = require("crypto-js/sha256");
-let data = SHA256("Vidit").toString();
-console.log(data);
-let name = "Vidi";
-hashName = SHA256(name).toString();
-console.log(hashName);
-console.log(hashName == data);
--- assignment/app.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const route = require("./routes");
-const server = http.createServer(route);
-server.listen(3003, () => {
-  console.log("Listening to Port 3003");
-});
