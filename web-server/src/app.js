9997cc2507115472cb0d3b35d5028e77ed152c7d
Vidit Shah <vidit0210@gmail.com>
2019-04-06 12:30:28 +0530

    Static Files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,2 +1,21 @@
+const path = require('path');
 const express = require('express');
-const app = express();
+const app = express(); {
+    // app.get('', (req, res) => {
+    //     res.send('Home Page');
+    //servinng static way --- Index.html has a special meaning..
+    // })
+}
+
+const pathToPublicDirectory = path.join(__dirname, '../public')
+app.use(express.static(pathToPublicDirectory))
+
+app.get('/weather', (req, res) => {
+    res.send([{
+        location: 'Mumbai',
+        temp: 33
+    }])
+})
+app.listen(3000, () => {
+    console.log('Listening to Port 3000')
+})
